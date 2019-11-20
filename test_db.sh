984c827669a9ed251dcd2bc110f8bbe94791b470
TanayaChivate <tanayachivate@gmail.com>
2019-11-20 16:57:52 +0530

    Changes done in shell script :)

--- test_db.sh
+++ test_db.sh
@@ -4,9 +4,9 @@
 # npm install
 
 # remove the container if exists or running 
-if [ $(docker container ls -q -a --filter name=mydb_img) != '' ]; then
-    docker container stop mydb_img
-    docker container rm mydb_img
+if [ $(docker container ls -q -a --filter name=mydb_c) != '' ]; then
+    docker container stop mydb_c
+    docker container rm mydb_c
 fi
 
 # remove the image if exists
@@ -18,4 +18,4 @@ fi
 docker image build -t mydb .
 
 # start the container
-docker container run -itd -p 4000:4000 --name mydb_imgc mydb
+docker container run -itd -p 4000:4000 --name mydb_c mydb
